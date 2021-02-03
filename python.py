72438ee9143ecbf035be72155a86dbc344876dcd
ayushparolia1989 <71411621+ayushparolia1989@users.noreply.github.com>
2021-02-04 01:28:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,14 @@
-num = float(input("Enter a number: "))
-if num >= 0:
-   if num == 0:
-       print("Zero")
-   else:
-       print("Positive number")
-else:
-   print("Negative number")
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2014  # year
+mm = 11    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
